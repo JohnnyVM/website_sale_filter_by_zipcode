61e0072bf5673258723ea9eb2fd09e2c0ad3abaa
johnny <vmjuan90@gmail.com>
2020-12-19 16:23:02 +0100

    first dunctional commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Filter product shop by zip code",
+    "summary": "Filter product shop by zip code",
+    "version": "0.1.0",
+    "author": "Juan Vázquez Moreno <vmjuan90@gmail.com>",
+    "website": "https://guadalstore.com",
+    "category": "Website",
+    "depends": ["website_sale"],
+    "data": [
+        "views/website_templates.xml",
+        ],
+    "license": "AGPL-3",
+    "installable": True,
+}
